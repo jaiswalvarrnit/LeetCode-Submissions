c352169fd389ddc679a9940594485b6d4efc48ed
Varrnit Jaiswal <67279489+jaiswalvarrnit@users.noreply.github.com>
2023-05-02 00:59:51 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -11,7 +11,7 @@ class Solution
     int knapSack(int W, int wt[], int val[], int n) 
     { 
        // Your code here
-       
+       // hehhehhehe
        int dp[n+1][W+1];
        
        for(int i = 0;i<n+1;i++){
